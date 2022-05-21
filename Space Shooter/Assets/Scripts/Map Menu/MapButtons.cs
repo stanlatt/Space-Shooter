870feb4dd2db5fa458df55dbc7a6170ee585079b
Mulin_Vlad <mulinvlad@gmail.com>
2022-05-21 18:29:16 +0300

    123

--- Space Shooter/Assets/Scripts/Map Menu/MapButtons.cs
+++ Space Shooter/Assets/Scripts/Map Menu/MapButtons.cs
@@ -1,17 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MapButtons : MonoBehaviour
 {
-    [Header("Two switchblade windows")]
+    [Header("Two switchblade windows: mapMenu and shop")]
     public GameObject mainPart;
     public GameObject shopWindow;
     bool mainPartActive;
     bool shopWindowActive;
 
+    [Header("TopMenu references")]
+    public Animator openCloseAnimator;
+    public bool topMenuOpen;
+
+    public GameObject topMenu;
+    bool topMenuActive;
+    public GameObject optionMenu;
+    bool optionMenuActive;
+
+
+
     private void Start()
     {
+        topMenuActive = true;
+        topMenuOpen = false;
+        optionMenuActive = false;
+
         mainPartActive = true;
         shopWindowActive = false; 
     }
@@ -20,6 +35,9 @@ public class MapButtons : MonoBehaviour
     {
         mainPart.gameObject.SetActive(mainPartActive);
         shopWindow.gameObject.SetActive(shopWindowActive);
+
+        topMenu.gameObject.SetActive(topMenuActive);
+        optionMenu.gameObject.SetActive(optionMenuActive);
     }
 
     public void Map_SHOP_ButtonClick()
@@ -27,17 +45,43 @@ public class MapButtons : MonoBehaviour
         mainPartActive = !mainPartActive;
         shopWindowActive = !shopWindowActive;
     }
-
     public void Shop_BACK_Button()
     {
         mainPartActive = !mainPartActive;
         shopWindowActive = !shopWindowActive;
     }
 
-  
+    
+    public void TopMenu_OpenClose_ButtonClick()
+    {
+        if(topMenuOpen == false)
+        {
+            openCloseAnimator.Play("Open");
+            topMenuOpen = true;
+        }
+        else
+        {
+            openCloseAnimator.Play("Close");
+            topMenuOpen = false;
+        }
 
+        
+    }
 
+    public void TopMenu_Options_ButtonClick()
+    {
+        optionMenuActive = !optionMenuActive;
+        topMenuActive = !topMenuActive;
+    }
 
+    public void TopMenu_OptionsBack_ButtonClick()
+    {
+        optionMenuActive = !optionMenuActive;
+        topMenuActive = !topMenuActive;
+    }
 
-
+    public void TopMenu_Back_ButtonClick()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
 }
